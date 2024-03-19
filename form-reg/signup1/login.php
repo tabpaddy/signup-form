26cb08a93adedfce673b42d05c8366cb205fed12
smallboyintech <111036308+tabpaddy@users.noreply.github.com>
2024-03-19 22:04:19 +0100

    Add files via upload

--- form-reg/signup1/login.php
+++ form-reg/signup1/login.php
@@ -1,7 +1,10 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-$success = 0;
-$user = 0;
+
+$login = 0;
+$invalid = 0;
 
   if ($_SERVER['REQUEST_METHOD']=='POST') {
     # code...
@@ -9,39 +12,18 @@ $user = 0;
     $user = $_POST['user'];
     $pass = $_POST['pass'];
 
-    // $sql = "INSERT INTO registration (user, pass)
-    //         VALUES('$user', '$pass')";
-
-    // $result = mysqli_query($conn, $sql);
-
-    // if ($result) {
-    //   # code...
-    //   echo "Data inserted successfully";
-    // }else {
-    //   die( mysqli_connect_error());
-    // }
-
-    $sql = "SELECT * FROM registration WHERE user = '$user'";
+    $sql = "SELECT * FROM registration WHERE user = '$user' and pass = '$pass'";
 
     $result = mysqli_query($conn, $sql);
     if ($result) {
         $num = mysqli_num_rows($result);
         if ($num > 0) {
-            // echo "user already exist";
-            $user = 1;
+           $login = 1;
+           session_start();
+           $_SESSION['user']=$user;
+           header('location:home.php');
         }else {
-          $sql = "INSERT INTO registration (user, pass)
-            VALUES('$user', '$pass')";
-
-        $result = mysqli_query($conn, $sql);
-        if ($result) {
-          # code...
-          // echo "Signup successful";
-          $success = 1;
-          header('location:login.php');
-        }else {
-          die( mysqli_error($conn));
-        }
+          $invalid = 1; 
         }
     }
   }
@@ -52,29 +34,31 @@ $user = 0;
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Signup page</title>
+    <title>Login page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
   </head>
   <body>
-    <?php
-        if ($user) {
-         echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-         <strong>Ohh no sorry </strong> User already exit.
-         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-       </div>';
-        }
-    ?>
-    <?php
-        if ($success) {
+  <?php
+        if ($login) {
          echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
-         <strong>Success </strong> You are successfully signup.
+         <strong>Success </strong> You are successfully logged in.
          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
        </div>';
         }
     ?>
-    <h1 class="text-center">Signup Page</h1>
+
+<?php
+        if ($invalid) {
+         echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+         <strong>Error </strong> Invalid Data.
+         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+       </div>';
+        }
+    ?>
+    
+    <h1 class="text-center">Login to our website</h1>
     <div class="container mt-5">
-    <form action="index.php" method="post">
+    <form action="login.php" method="post">
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Name</label>
     <input type="text" class="form-control"  placeholder="Enter your username" name="user">
@@ -85,7 +69,7 @@ $user = 0;
     <input type="password" class="form-control" placeholder="Enter your password" name="pass">
   </div>
   
-  <button type="submit" class="btn btn-primary w-100">Signup</button>
+  <button type="submit" class="btn btn-primary w-100">Login</button>
 </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
